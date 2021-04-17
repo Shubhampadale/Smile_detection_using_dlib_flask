7ada462830193fbcb5463d6df8795f1ddce01110
Shubhampadale <spadale0999@gmail.com>
2021-04-17 14:57:44 +0530

    Updated2

--- camera.py
+++ camera.py
@@ -35,7 +35,7 @@ def faceLandmarkPoints(image, faceLandmarks):
         
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0)
+        self.video = cv2.VideoCapture(-1)
     
     def __del__(self):
         self.video.release()
